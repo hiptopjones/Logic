d4f59433b7382f70527e04c70a61133b994dd4a6
hiptopjones <github@flinch.ca>
2014-11-06 23:01:53 -0800

    Add a 16-bit adder and tests.

---
+++ Logic/Adders/WordAdder.cs
@@ -0,0 +1,80 @@
+﻿using Logic.Components;
+using Logic.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic.Adders
+{
+    public class WordAdder
+    {
+        private ByteAdder LowByteAdder { get; set; }
+        private ByteAdder HighByteAdder { get; set; }
+
+        public Node InputA0 { get { return LowByteAdder.InputA0; } }
+        public Node InputA1 { get { return LowByteAdder.InputA1; } }
+        public Node InputA2 { get { return LowByteAdder.InputA2; } }
+        public Node InputA3 { get { return LowByteAdder.InputA3; } }
+        public Node InputA4 { get { return LowByteAdder.InputA4; } }
+        public Node InputA5 { get { return LowByteAdder.InputA5; } }
+        public Node InputA6 { get { return LowByteAdder.InputA6; } }
+        public Node InputA7 { get { return LowByteAdder.InputA7; } }
+        public Node InputA8 { get { return HighByteAdder.InputA0; } }
+        public Node InputA9 { get { return HighByteAdder.InputA1; } }
+        public Node InputA10 { get { return HighByteAdder.InputA2; } }
+        public Node InputA11 { get { return HighByteAdder.InputA3; } }
+        public Node InputA12 { get { return HighByteAdder.InputA4; } }
+        public Node InputA13 { get { return HighByteAdder.InputA5; } }
+        public Node InputA14 { get { return HighByteAdder.InputA6; } }
+        public Node InputA15 { get { return HighByteAdder.InputA7; } }
+
+        public Node InputB0 { get { return LowByteAdder.InputB0; } }
+        public Node InputB1 { get { return LowByteAdder.InputB1; } }
+        public Node InputB2 { get { return LowByteAdder.InputB2; } }
+        public Node InputB3 { get { return LowByteAdder.InputB3; } }
+        public Node InputB4 { get { return LowByteAdder.InputB4; } }
+        public Node InputB5 { get { return LowByteAdder.InputB5; } }
+        public Node InputB6 { get { return LowByteAdder.InputB6; } }
+        public Node InputB7 { get { return LowByteAdder.InputB7; } }
+        public Node InputB8 { get { return HighByteAdder.InputB0; } }
+        public Node InputB9 { get { return HighByteAdder.InputB1; } }
+        public Node InputB10 { get { return HighByteAdder.InputB2; } }
+        public Node InputB11 { get { return HighByteAdder.InputB3; } }
+        public Node InputB12 { get { return HighByteAdder.InputB4; } }
+        public Node InputB13 { get { return HighByteAdder.InputB5; } }
+        public Node InputB14 { get { return HighByteAdder.InputB6; } }
+        public Node InputB15 { get { return HighByteAdder.InputB7; } }
+
+        public Node Output0 { get { return LowByteAdder.Output0; } }
+        public Node Output1 { get { return LowByteAdder.Output1; } }
+        public Node Output2 { get { return LowByteAdder.Output2; } }
+        public Node Output3 { get { return LowByteAdder.Output3; } }
+        public Node Output4 { get { return LowByteAdder.Output4; } }
+        public Node Output5 { get { return LowByteAdder.Output5; } }
+        public Node Output6 { get { return LowByteAdder.Output6; } }
+        public Node Output7 { get { return LowByteAdder.Output7; } }
+        public Node Output8 { get { return HighByteAdder.Output0; } }
+        public Node Output9 { get { return HighByteAdder.Output1; } }
+        public Node Output10 { get { return HighByteAdder.Output2; } }
+        public Node Output11 { get { return HighByteAdder.Output3; } }
+        public Node Output12 { get { return HighByteAdder.Output4; } }
+        public Node Output13 { get { return HighByteAdder.Output5; } }
+        public Node Output14 { get { return HighByteAdder.Output6; } }
+        public Node Output15 { get { return HighByteAdder.Output7; } }
+
+        public Node InputCarry { get { return LowByteAdder.InputCarry; } }
+        public Node OutputCarry { get { return HighByteAdder.OutputCarry; } }
+
+        public WordAdder()
+        {
+            LowByteAdder = new ByteAdder();
+            HighByteAdder = new ByteAdder();
+
+            LowByteAdder.OutputCarry.AttachSink(HighByteAdder.InputCarry);
+
+            InstanceCounter.Add(GetType());
+        }
+    }
+}
---
+++ UnitTests/WordAdderTests.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Logic;
+using Logic.Adders;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class WordAdderTests
+    {
+        [TestMethod]
+        public void WordAdder_Constructor_Defaults()
+        {
+            // Arrange / Act
+            WordAdder wordAdder = new WordAdder();
+
+            // Assert
+            AssertHasValueAndCarry(0, false, wordAdder);
+        }
+
+        [TestMethod]
+        public void WordAdder_InputAIs1_OutputIs1()
+        {
+            // Arrange
+            WordAdder wordAdder = new WordAdder();
+
+            // Act
+            SetInputAValue(wordAdder, 1);
+
+            // Assert
+            AssertHasValueAndCarry(1, false, wordAdder);
+        }
+
+        [TestMethod]
+        public void WordAdder_InputB0IsTrue_OutputIs1()
+        {
+            // Arrange
+            WordAdder wordAdder = new WordAdder();
+
+            // Act
+            SetInputBValue(wordAdder, 1);
+
+            // Assert
+            AssertHasValueAndCarry(1, false, wordAdder);
+        }
+
+        [TestMethod]
+        public void WordAdder_InputA0AndB0IsTrue_OutputIs2()
+        {
+            // Arrange
+            WordAdder wordAdder = new WordAdder();
+
+            // Act
+            SetInputAValue(wordAdder, 1);
+            SetInputBValue(wordAdder, 1);
+
+            // Assert
+            AssertHasValueAndCarry(2, false, wordAdder);
+        }
+
+        [TestMethod]
+        public void WordAdder_InputA0AndB0AndCarryIsTrue_OutputIs3()
+        {
+            // Arrange
+            WordAdder wordAdder = new WordAdder();
+
+            // Act
+            SetInputAValue(wordAdder, 1);
+            SetInputBValue(wordAdder, 1);
+            wordAdder.InputCarry.Value = true;
+
+            // Assert
+            AssertHasValueAndCarry(3, false, wordAdder);
+        }
+
+        [TestMethod]
+        public void WordAdder_InputAIs65535_OutputIs65535()
+        {
+            // Arrange
+            WordAdder wordAdder = new WordAdder();
+
+            // Act
+            SetInputAValue(wordAdder, 65535);
+
+            // Assert
+            AssertHasValueAndCarry(65535, false, wordAdder);
+        }
+
+        [TestMethod]
+        public void WordAdder_InputAIs65535AndBIs1AndCarry_OutputIs0WithCarry()
+        {
+            // Arrange
+            WordAdder wordAdder = new WordAdder();
+
+            // Act
+            SetInputAValue(wordAdder, 65535);
+            SetInputBValue(wordAdder, 1);
+
+            // Assert
+            AssertHasValueAndCarry(0, true, wordAdder);
+        }
+
+        [TestMethod]
+        public void WordAdder_InputAIs32768AndBIs32768_OutputIs0WithCarry()
+        {
+            // Arrange
+            WordAdder wordAdder = new WordAdder();
+
+            // Act
+            SetInputAValue(wordAdder, 32768);
+            SetInputBValue(wordAdder, 32768);
+
+            // Assert
+            AssertHasValueAndCarry(0, true, wordAdder);
+        }
+
+        [TestMethod]
+        public void WordAdder_InputAIs255AndBIs1_OutputIs256()
+        {
+            // Arrange
+            WordAdder wordAdder = new WordAdder();
+
+            // Act
+            SetInputAValue(wordAdder, 255);
+            SetInputBValue(wordAdder, 1);
+
+            // Assert
+            AssertHasValueAndCarry(256, false, wordAdder);
+        }
+
+        private void SetInputAValue(WordAdder wordAdder, int inputValue)
+        {
+            wordAdder.InputA0.Value = (((inputValue >> 0) & 1) == 1);
+            wordAdder.InputA1.Value = (((inputValue >> 1) & 1) == 1);
+            wordAdder.InputA2.Value = (((inputValue >> 2) & 1) == 1);
+            wordAdder.InputA3.Value = (((inputValue >> 3) & 1) == 1);
+            wordAdder.InputA4.Value = (((inputValue >> 4) & 1) == 1);
+            wordAdder.InputA5.Value = (((inputValue >> 5) & 1) == 1);
+            wordAdder.InputA6.Value = (((inputValue >> 6) & 1) == 1);
+            wordAdder.InputA7.Value = (((inputValue >> 7) & 1) == 1);
+            wordAdder.InputA8.Value = (((inputValue >> 8) & 1) == 1);
+            wordAdder.InputA9.Value = (((inputValue >> 9) & 1) == 1);
+            wordAdder.InputA10.Value = (((inputValue >> 10) & 1) == 1);
+            wordAdder.InputA11.Value = (((inputValue >> 11) & 1) == 1);
+            wordAdder.InputA12.Value = (((inputValue >> 12) & 1) == 1);
+            wordAdder.InputA13.Value = (((inputValue >> 13) & 1) == 1);
+            wordAdder.InputA14.Value = (((inputValue >> 14) & 1) == 1);
+            wordAdder.InputA15.Value = (((inputValue >> 15) & 1) == 1);
+        }
+
+        private void SetInputBValue(WordAdder wordAdder, int inputValue)
+        {
+            wordAdder.InputB0.Value = (((inputValue >> 0) & 1) == 1);
+            wordAdder.InputB1.Value = (((inputValue >> 1) & 1) == 1);
+            wordAdder.InputB2.Value = (((inputValue >> 2) & 1) == 1);
+            wordAdder.InputB3.Value = (((inputValue >> 3) & 1) == 1);
+            wordAdder.InputB4.Value = (((inputValue >> 4) & 1) == 1);
+            wordAdder.InputB5.Value = (((inputValue >> 5) & 1) == 1);
+            wordAdder.InputB6.Value = (((inputValue >> 6) & 1) == 1);
+            wordAdder.InputB7.Value = (((inputValue >> 7) & 1) == 1);
+            wordAdder.InputB8.Value = (((inputValue >> 8) & 1) == 1);
+            wordAdder.InputB9.Value = (((inputValue >> 9) & 1) == 1);
+            wordAdder.InputB10.Value = (((inputValue >> 10) & 1) == 1);
+            wordAdder.InputB11.Value = (((inputValue >> 11) & 1) == 1);
+            wordAdder.InputB12.Value = (((inputValue >> 12) & 1) == 1);
+            wordAdder.InputB13.Value = (((inputValue >> 13) & 1) == 1);
+            wordAdder.InputB14.Value = (((inputValue >> 14) & 1) == 1);
+            wordAdder.InputB15.Value = (((inputValue >> 15) & 1) == 1);
+        }
+
+        private int GetOutputValue(WordAdder wordAdder)
+        {
+            int actualValue = 0;
+
+            actualValue |= (wordAdder.Output0.Value ? 1 << 0 : 0);
+            actualValue |= (wordAdder.Output1.Value ? 1 << 1 : 0);
+            actualValue |= (wordAdder.Output2.Value ? 1 << 2 : 0);
+            actualValue |= (wordAdder.Output3.Value ? 1 << 3 : 0);
+            actualValue |= (wordAdder.Output4.Value ? 1 << 4 : 0);
+            actualValue |= (wordAdder.Output5.Value ? 1 << 5 : 0);
+            actualValue |= (wordAdder.Output6.Value ? 1 << 6 : 0);
+            actualValue |= (wordAdder.Output7.Value ? 1 << 7 : 0);
+            actualValue |= (wordAdder.Output8.Value ? 1 << 8 : 0);
+            actualValue |= (wordAdder.Output9.Value ? 1 << 9 : 0);
+            actualValue |= (wordAdder.Output10.Value ? 1 << 10 : 0);
+            actualValue |= (wordAdder.Output11.Value ? 1 << 11 : 0);
+            actualValue |= (wordAdder.Output12.Value ? 1 << 12 : 0);
+            actualValue |= (wordAdder.Output13.Value ? 1 << 13 : 0);
+            actualValue |= (wordAdder.Output14.Value ? 1 << 14 : 0);
+            actualValue |= (wordAdder.Output15.Value ? 1 << 15 : 0);
+
+            return actualValue;
+        }
+
+        private void AssertHasValueAndCarry(int expectedValue, bool carry, WordAdder wordAdder)
+        {
+            int actualValue = GetOutputValue(wordAdder);
+
+            Assert.AreEqual(expectedValue, actualValue);
+            Assert.AreEqual(carry, wordAdder.OutputCarry.Value);
+        }
+    }
+}
